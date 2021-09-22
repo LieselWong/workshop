2ed4ab62d17ee9362d3df6562448b4d9ecde0012
LieselWong <68936656+LieselWong@users.noreply.github.com>
2021-09-22 09:25:20 -0400

    update with header

--- 01lab-Pythonpd1names.py
+++ 01lab-Pythonpd1names.py
@@ -1,3 +1,8 @@
+#Liesel Wong
+#SoftDev
+#K03 -- PythonNames/Python/Code which prints a random name from period 1 soft dev, and a random name from pd 2 soft dev
+#2021-9-22
+
 import random
 #Lists of period 1 and 2
 pd1 = ['Owen Yaggy', 'Haotain Gan', 'Ishraq Mahid', 'Kevin Cao', 'Ivan Lam', 'Michelle Lo', 'Christopher Liu', 'Michael Borzuk', 'Ivan Mijacika', 'Lucas Lee', 'Rayat Roy', 'Emma Buller', 'Andrew Juang', 'Renggeng Zheng', 'Annabel Zhang', 'Alejandro Alonso', 'Deven Mahehwari', 'David', 'Aryaman Goenka', 'Oscar Wang', 'Tami Takada', 'Yusuf Elsharawy', 'Ella Krechmer', 'Tomas Acuna', 'Tina Nguyen', 'Sadid Ethun', 'Shyne Choi', 'Shriya Anand', 'lol28', 'lol29', 'lol30', 'lol31']
