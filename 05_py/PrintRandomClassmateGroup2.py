43f6b208b0e7d46a7bf1ed2302fb504b0852557b
LieselWong <68936656+LieselWong@users.noreply.github.com>
2021-09-27 21:46:37 -0400

    Update PrintRandomClassmateGroup2.py
    update with new dictionary key python format

--- 05_py/PrintRandomClassmateGroup2.py
+++ 05_py/PrintRandomClassmateGroup2.py
@@ -10,18 +10,18 @@
 
 #Start of code
 import random
-#The rest of the comments explain each subsection of code.
 
-#Lists containing names of people in each period
-pd1 = ['Owen Yaggy', 'Haotain Gan', 'Ishraq Mahid', 'Ivan Lam', 'Michelle Lo', 'Christopher Liu', 'Michael Borzuk', 'Ivan Mijacika', 'Lucas Lee', 'Rayat Roy', 'Emma Buller', 'Andrew Juang', 'Renggeng Zheng', 'Annabel Zhang', 'Alejandro Alonso', 'Deven Mahehwari', 'David', 'Aryaman Goenka', 'Oscar Wang', 'Tami Takada', 'Yusuf Elsharawy', 'Ella Krechmer', 'Tomas Acuna', 'Tina Nguyen', 'Sadid Ethun', 'Shyne Choi', 'Shriya Anand']
-pd2 = ['Patrick Ging', 'Raymond Yeung', 'Josephine Lee', 'Alif Abdullah', 'William Chen', 'Justin Zou', 'Andy Lin', 'Rachel Xiao', 'Yuqing Wu', 'Shadman Rakib', 'Liesel Wong', 'Daniel Sooknanan', 'Cameron Nelson', 'Austin Ngan', 'Thomas Yu', 'Yaying Liang Li', 'Jesse Xie', 'Eric Guo', 'Jonathan Wu', 'Zhaoyu Lin', 'Joshua Kloepfer', 'Noakai Aronesty', 'Yoonah Chang', 'Hebe Huang', 'Wen Hao Dong', 'Mark Zhu', 'Kevin Cao']
+#Dictionary containing names of people in each period
+names = {
+    'pd1' : ['Owen Yaggy', 'Haotain Gan', 'Ishraq Mahid', 'Ivan Lam', 'Michelle Lo', 'William Chen', 'Christopher Liu', 'Michael Borzuk', 'Ivan Mijacika', 'Lucas Lee', 'Rayat Roy', 'Emma Buller', 'Andrew Juang', 'Renggeng Zheng', 'Annabel Zhang', 'Alejandro Alonso', 'Deven Mahehwari', 'David', 'Aryaman Goenka', 'Oscar Wang', 'Tami Takada', 'Yusuf Elsharawy', 'Ella Krechmer', 'Tomas Acuna', 'Tina Nguyen', 'Sadid Ethun', 'Shyne Choi', 'Shriya Anand'],
+    'pd2' : ['Patrick Ging', 'Raymond Yeung', 'Josephine Lee', 'Alif Abdullah', 'Justin Zou', 'Andy Lin', 'Rachel Xiao', 'Yuqing Wu', 'Shadman Rakib', 'Liesel Wong', 'Daniel Sooknanan', 'Cameron Nelson', 'Austin Ngan', 'Thomas Yu', 'Yaying Liang Li', 'Jesse Xie', 'Eric Guo', 'Jonathan Wu', 'Zhaoyu Lin', 'Joshua Kloepfer', 'Noakai Aronesty', 'Yoonah Chang', 'Hebe Huang', 'Wen Hao Dong', 'Mark Zhu', 'Kevin Cao']
+}
+#Variables used to hold randomly selected indeces from both lists in the names dictionary
+pd1Chosen = random.randint(0,len(names['pd1'])-1)
+pd2Chosen = random.randint(0,len(names['pd2'])-1)
 
-#Variables used to hold randomly selected indeces from both lists
-pd1Chosen = random.randint(0,len(pd1)-1)
-pd2Chosen = random.randint(0,len(pd2)-1)
-
-#Printing a randomly selected name, chosen by the variables above, from one of the two lists, dictated by the conditional statements below.
+#Printing a randomly selected name, chosen by the variables above, from one of the two lists within the names dictionary above, as determined by the code below.
 if (random.randint(0, 1) == 0):
-    print("Period 1 Name: " + pd1[pd1Chosen])
+    print("Period 1 Name: " + names['pd1'][pd1Chosen])
 else:
-    print("Period 2 Name: " + pd1[pd1Chosen])
+    print("Period 2 Name: " + names['pd2'][pd2Chosen])
