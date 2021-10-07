a726bd23151bf4669fc35747eefe7ffe3fb6e71a
LieselWong <awesomesaucelw@gmail.com>
2021-10-07 18:46:16 -0400

    Update with k12

--- 12_flask-jinja/app.py
+++ 12_flask-jinja/app.py
@@ -1,19 +1,20 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2021
+#Team Untitled - Jonathan W. , Liesel W. , Loki , King Hagrid
+#SoftDev
+#K12 -- ...or The Only Constant is Change?/exploring jinja2/
+# 2021-10--07
 
-from flask import Flask, render_template #Q0: What happens if you remove render_template from this line?
+from flask import Flask, render_template#Q0: What happens if you remove render_template from this line?
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "No hablo queso!"
+    return "No hablo queso!" 
 
 coll = [0,1,1,2,3,5,8]
 
 @app.route("/my_foist_template") #Q1: Can all of your teammates confidently predict the URL to use to load this page?
 def test_tmplt():
-    return render_template( 'model_tmplt.html', foo="fooooo", collection=coll) #Q2: What is the significance of each argument?
+    return render_template( 'model_tmplt.html', foo="foooo", collection=coll) #Q2: What is the significance of each argument?
 
 if __name__ == "__main__":
     app.debug = True
