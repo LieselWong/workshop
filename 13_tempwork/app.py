01f8ca4b992fff43a65fbfc49f6ebf7d4d0f1172
LieselWong <awesomesaucelw@gmail.com>
2021-10-12 16:40:24 -0400

    add extra part lesson 13

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -14,22 +14,27 @@ def hello_world():
 
 occupations = {}
 
+
 with open('data/occupations.csv') as csv_file: #give access to the file by opening it
     reader = csv.reader(csv_file, delimiter=',') #delimeter puts values in a list for each row as it splits by commas
     for row in reader:
         if row[0] != "Job Class": #don't store the heading or total
-            occupations[row[0]] = float(row[1]) #put the data in a dictionary
-            
+            occupations[row[0]] = [float(row[1]), row[2]] #put the data in a dictionary
+
             
 def make_choice():
     jobs = list(occupations.keys()) # convert the keys into a list
     jobs.pop(len(jobs)-1)
-    percentages = list(occupations.values())
-    percentages.pop(len(percentages)-1)
+    values = list(occupations.values())
+    values.pop(len(values)-1)
+    percentages = []
+    for arr in values:
+        percentages.append(arr[0])
     choice = random.choices(jobs, weights = percentages, k=1)
-    return choice[0] 
+    return choice[0] + " | Link: " + occupations[choice[0]][1]
 
 @app.route("/occupyflaskst")
+
 def test_tmplt():
     return render_template('tablified.html', dict = occupations, selected_occupation = make_choice())
 
