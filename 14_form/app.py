03d3b84bf560c59fac499075dc0bd51e716fcf83
LieselWong <awesomesaucelw@gmail.com>
2021-10-15 09:22:56 -0400

    update request.method

--- 14_form/app.py
+++ 14_form/app.py
@@ -1,6 +1,7 @@
-# Clyde 'Thluffy' Sinclair
+# Team Marshmallow: Liesel Wong (King Hagrid), Yoonah Chang (Yelena), Rachel Xiao (Mooana)
 # SoftDev
-# Oct 2021 
+# K14 -- Form and Function
+# 2021-10-14 
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -26,13 +27,15 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app) # prints <Flask 'app'> in the terminal
+    print(app) # <Flask 'app'>
     print("***DIAG: request obj ***")
-    print(request) # <Request 'http://127.0.0.1:5000/auth?username=&sub1=Submit' [GET]>
+    print(request) # <Request 'http://127.0.0.1:5000/' [GET]> --after submitting form--> <Request 'http://127.0.0.1:5000/auth?username=hello&sub1=Submit+Query' [GET]>
+                   # request shows the url of the localhost when tracking the form data
     print("***DIAG: request.args ***")
-    print(request.args) # ImmutableMultiDict([('username', ''), ('sub1', 'Submit')])
+    print(request.args) # ImmutableMultiDict([]) --> ImmutableMultiDict([('username', 'hello'), ('sub1', 'Submit Query')])  |  (this creates a dictionary with
+                        # [name of input tag: user input] 
     #print("***DIAG: request.args['username']  ***")
-    #print(request.args['username'])
+    #print(request.args['username']) # hello  |  (this prints the inputted username)
     print("***DIAG: request.headers ***")
     print(request.headers) # Host: 127.0.0.1:5000
     return render_template( 'login.html' )
@@ -51,11 +54,11 @@ def authenticate():
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return "Waaaa hooo HAAAH"  #response to a form submission
+    return render_template('response.html', username=request.args['username'], method=request.method)  #response to a form submission
 
 
-    
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+    app.debug = True
     app.run()
